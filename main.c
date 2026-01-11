19036930f7197ac8374df3988c60891059e6e76d
Selim Efe <uysalselimefe@gmail.com>
2026-01-11 12:03:26 +0300

    Implement logic for self-power series calculation
    Added source code that utilizes nested loops to calculate the exponential growth series ($1^1, 2^2, 3^3...$) up to user-defined limit n.

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int j,i,sonuc,n;
+
+    printf("n i gir ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+
+        sonuc=1;
+
+        for(j=1;j<=i;j++){
+            sonuc=sonuc*i;
+        }
+        printf("%d ",sonuc);
+    }
+
+    return 0;
+}
